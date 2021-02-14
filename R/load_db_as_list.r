be0b3ac778aff6f248418b548106eb7f52c1c7e5
Daniel Hekman <hekman@ADS.SSC.WISC.EDU>
2021-02-14 13:32:12 -0600

    :shower: cleaning house

---
+++ R/load_db_as_list.r
@@ -0,0 +1,14 @@
+load_db_as_list <- function(
+  user_file = NA,
+  hashtag_file = NA,
+  tweet_file = NA,
+  url_file = NA
+) {
+  user_file = NA,
+  hashtag_file = NA,
+  tweet_file = NA,
+  url_file = NA
+  
+  
+  
+}
---
+++ runme.bat
@@ -0,0 +1,3 @@
+pip install tweepy
+pip install psycopg2
+python.exe Z:\covid_gov_tweets\Python\populate_db_from_twitter.py
