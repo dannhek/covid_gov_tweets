8d9419c1887de932e4c6ed36346f68564dd220a0
Daniel Hekman <hekman@ADS.SSC.WISC.EDU>
2021-02-14 13:39:25 -0600

    syntax

--- R/load_db_as_list.r
+++ R/load_db_as_list.r
@@ -4,11 +4,13 @@ load_db_as_list <- function(
   tweet_file = NA,
   url_file = NA
 ) {
-  if (is.na(user_file)) { user_file <- file.path('.','db','user_file.csv')  
-  if (is.na(hashtag_file)) { hashtag_file <- file.path('.','db','hashtag_file.csv')  
-  if (is.na(tweet_file)) { tweet_file <- file.path('.','db','tweet_file.csv')  
-  if (is.na(url_file)) { url_file <- file.path('.','db','url_file.csv') 
+  if (is.na(user_file)) { user_file <- file.path('.','db','atusers.csv') }
+  if (is.na(hashtag_file)) { hashtag_file <- file.path('.','db','hashtags.csv') }  
+  if (is.na(tweet_file)) { tweet_file <- file.path('.','db','tweets.csv') }
+  if (is.na(url_file)) { url_file <- file.path('.','db','urlrefs.csv') }
   
-  
+  ret <- list(
+    
+  )
   
 }
