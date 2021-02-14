f930e3e95cb877e52b9383f70b3f0931c59e308d
Dann Hekman <dannhek@gmail.com>
2021-02-14 13:37:25 -0600

    if is na

--- R/load_db_as_list.r
+++ R/load_db_as_list.r
@@ -4,10 +4,10 @@ load_db_as_list <- function(
   tweet_file = NA,
   url_file = NA
 ) {
-  user_file = NA,
-  hashtag_file = NA,
-  tweet_file = NA,
-  url_file = NA
+  if (is.na(user_file)) { user_file <- file.path('.','db','user_file.csv')  
+  if (is.na(hashtag_file)) { hashtag_file <- file.path('.','db','hashtag_file.csv')  
+  if (is.na(tweet_file)) { tweet_file <- file.path('.','db','tweet_file.csv')  
+  if (is.na(url_file)) { url_file <- file.path('.','db','url_file.csv') 
   
   
   
